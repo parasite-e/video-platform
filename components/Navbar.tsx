df4539a0603c467a14075d258a1b4f361725abe0
parasite-e <fardinemonbd@gmail.com>
2022-07-17 19:31:32 +0600

    navbar added

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+
+import LOGO from '../utils/tiktik-logo.png';
+
+export const Navbar = () => {
+    return (
+        <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+            <Link href="/">
+                <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
+                    <Image className='cursor-pointer' src={LOGO} alt='logo' layout='responsive' />
+                </div>
+            </Link>
+        </div>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
+import { Navbar } from '../components/Navbar';
+import { Sidebar } from '../components/Sidebar';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setSSR] = useState(true);
+
+  useEffect(() => {
+    setSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+          <Sidebar></Sidebar>
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+
+  );
 }
 
 export default MyApp
